d75f7ee810c9e7bb343c060eafd6e0e80a6c5a24
U-BSD\456523 <456523@SHS-J5W2NW3.beaverton.k12.or.us>
2025-09-05 15:38:09 -0700

    Completed basic outline; program can successfully check for palindromes!

--- main.cpp
+++ main.cpp
@@ -1,1 +1,42 @@
-//hi
+#include <iostream>
+#include <cstring>
+#include <cctype>
+
+using namespace std;
+
+int main() {
+	bool conditiono = true;
+	while (conditiono) {
+		cout << "Give me some text and I will tell you if it is a palindrome!\n> ";
+		char text[80];
+
+		cin.get(text, 80);
+		cin.get();
+		
+		int i = 0;
+		for (char c : text) {
+			if (isalnum(c)) {
+				text[i] = tolower(c);
+				i++;
+			}
+			if (c == '\0') {
+				break;
+			}
+		}
+		text[i] = '\0';
+
+		bool palindrome = true;
+		for (int j = 0; j < strlen(text) / 2; j++) {
+			if (text[j] != text[strlen(text) - j - 1]) {
+				palindrome = false;
+				break;
+			}
+		}
+
+		if (palindrome) {
+			cout << "Yes palindrome.";
+		} else {
+			cout << "Not a palindrome.";
+		}
+	}
+}
